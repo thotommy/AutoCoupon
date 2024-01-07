c098e4fa1a2238c812ce0e60ac7ebfb008facb72
thotommy <thotommy94@gmail.com>
2024-01-07 00:17:23 -0500

    Adding support for Publix and starting target. Troubleshooting buttons to clip coupons on Publix. Updated README.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This python project is an example of how we can use gkeepapi to grab a shopping 
 
 Current stores supported are:
 * Harris Teeters
+* Publix
+* Target (coming soon)
 
 To run, must use Python version 3.8.18 or 3.8.2
 
@@ -13,6 +15,12 @@ Create a .env file that has the following parameters:
 * KEEP_LIST="Google Keep ID" #(You can get this from opening the google keep note and seeing the long string in the url)
 * SHOP_USERID="Harris Teeter User email"
 * SHOP_PWD="Harris Teeter User password"
+* PUBLIX_USERID="Publix user name"
+* PUBLIX_PWD="Publix password"
+* PUBLIX_KEEP_LIST="Google Keep ID"
+* TARGET_USERID="Target User email"
+* TARGET_PWD="Target password"
+* TARGET_KEEP_LIST="Google Keep ID"
 
 Then run
 ```shell
--- main.py
+++ main.py
@@ -9,7 +9,12 @@ import os
 from selenium.webdriver.common.keys import Keys
 # Needs to use python version 3.8.18
 
-def get_list():
+# Constants
+HARRIS_TEETER = "harris teeter"
+PUBLIX = "publix"
+TARGET = "target"
+
+def get_list(grocery):
     load_dotenv()
     user = os.getenv('KEEP_USER')
     pwd = os.getenv('KEEP_PWD')
@@ -17,11 +22,24 @@ def get_list():
     success = keep.login(user, pwd)
 
     if(success == True):
-        gnotes = keep.get(os.getenv('KEEP_LIST'))
+        if grocery is HARRIS_TEETER:
+            print("Grabbing Harris Teeter list..")
+            gnotes = keep.get(os.getenv('KEEP_LIST'))
+        elif grocery is PUBLIX:
+            print("Grabbing Publix list..")
+            gnotes = keep.get(os.getenv('PUBLIX_KEEP_LIST'))
+        elif grocery is TARGET:
+            print("Grabbing Target list..")
+            gnotes = keep.get(os.getenv('TARGET_KEEP_LIST'))
+        else:
+            print("Grabbing default list..")
+            gnotes = keep.get(os.getenv('KEEP_LIST'))
+
         return f'{gnotes.title} \n\n{gnotes.text}'
 
 
 def login_harris_teeters(driver):
+    print("Login in to Harris Teeter..")
     load_dotenv()
     username = os.getenv('SHOP_USERID')
     pw = os.getenv('SHOP_PWD')
@@ -40,6 +58,7 @@ def login_harris_teeters(driver):
 
 
 def apply_harris_teeter_coupons(driver, list_arr):
+    print("Applying Harris Teeter Coupons..")
     driver.get("https://www.harristeeter.com/coupons/")
     sleep(5)
     for item in list_arr:
@@ -54,15 +73,82 @@ def apply_harris_teeter_coupons(driver, list_arr):
             continue
         for clip_button in clip_buttons:
             clip_button.click()
-    
+
+
+def login_publix(driver):
+    print("Login in to Publix..")
+    load_dotenv()
+    username = os.getenv('SHOP_USERID')
+    pw = os.getenv('SHOP_PWD')
+    driver.get("https://www.publix.com/")
+    sleep(3)
+    driver.find_element(By.XPATH, "//*[@id=\"userLogIn\"]").click()
+    sleep(5)
+    driver.find_element(By.XPATH, "//input[@name=\"Email address\"]").send_keys(username)
+    sleep(2)
+    driver.find_element(By.XPATH, "//input[@name=\"Password\"]").send_keys(pw)
+    sleep(1)
+    driver.find_element(By.XPATH, "//button[@id=\"next\"]").click()
+    sleep(10)
+
+
+def apply_publix_coupons(driver, list_arr):
+    print("Applying Publix coupons..")
+    driver.get("https://www.publix.com/savings/digital-coupons?nav=header")
+    sleep(5)
+    for item in list_arr:
+        if '==' in item:
+            continue
+        print(f'Finding coupons for {item}')
+        driver.find_element(By.XPATH, "/html/body/div[1]/section/div[4]/div/div[2]/div[1]/div[2]/div/div[1]/form/input").clear()
+        driver.find_element(By.XPATH, "/html/body/div[1]/section/div[4]/div/div[2]/div[1]/div[2]/div/div[1]/form/input").send_keys(item)
+        driver.find_element(By.XPATH, "/html/body/div[1]/section/div[4]/div/div[2]/div[1]/div[2]/div/div[1]/form/input").send_keys(Keys.ENTER)
+        sleep(4)
+        #TODO: Need to figure out how to get a list of Clip coupon buttons and click them using selenium
+        clip_buttons = driver.find_elements(By.XPATH, "//button[contains(text(), 'Clip coupon')]")
+        print(len(clip_buttons))
+        if(len(clip_buttons) == 0):
+            continue
+        for clip_button in clip_buttons:
+            clip_button.click()
+
+
+def login_target(driver):
+    print("Login in to Target..")
+    load_dotenv()
+    username = os.getenv('TARGET_USERID')
+    pw = os.getenv('TARGET_PWD')
+    driver.get('https://www.target.com/')
+    sleep(3)
+    driver.find_element(By.XPATH, "//*[@id=\"headerPrimary\"]/a[4]").click()
+    sleep(2)
+    driver.find_element(By.XPATH, "/html/body/div[9]/div/div/div[2]/ul/li[1]/a").click()
+    sleep(3)
+    driver.find_element(By.XPATH, "//input[@name=\"username\"]").send_keys(username)
+    sleep(2)
+    driver.find_element(By.XPATH, "//input[@name=\"password\"]").send_keys(pw)
+    sleep(1)
+    driver.find_element(By.XPATH, "//button[@id=\"login\"]").click()
+    sleep(10)
+
+
+def apply_target_coupons(driver, list_arr):
+    print("Applying Target coupons..")
+    '''
+    TODO: Need to find a good way to clip coupons in Target. At this time target 
+    does not have a great way to clip coupons.
+    '''
+    pass
 
 if __name__ == "__main__":
-    list = get_list()
+    
+    list = get_list(PUBLIX)
     lines = list.split('\n')
 
     # Remove ☑ or ☐ from each line
     list_arr = [line.replace('☑', '').replace('☐', '').strip() for line in lines]
     print(list_arr)
+    
     
     print('Starting Chrome Driver')
     options = webdriver.ChromeOptions()
@@ -71,6 +157,11 @@ if __name__ == "__main__":
     driver = uc.Chrome(options=options)
     driver.maximize_window()
 
+    login_publix(driver)
+    apply_publix_coupons(driver, list_arr)
+
+    ''' 
     login_harris_teeters(driver)
     apply_harris_teeter_coupons(driver, list_arr)
+    '''
 
